6ea9f8bfd742b28319899ef685441b0515ee95e3
gabrielfel1x <gabrielfelxx@gmail.com>
2024-05-08 09:28:37 -0300

    redirect to page login

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -16,14 +16,17 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "vue-router";
 
 const loggedIn = ref(false);
+const router = useRouter();
 
 const signOutUser = async () => {
   const auth = getAuth();
   try {
     await signOut(auth);
     loggedIn.value = false;
+    router.push({ path: "/" });
     console.log("Deslogado com sucesso");
   } catch (error) {
     console.error("Erro ao deslogar:", error);
